9a75726228c13f88e7835af859dba343d877ccec
tabbbyzzxc <ipz221_mms@student.ztu.edu.ua>
2023-11-25 14:10:49 +0100

    test

---
+++ ATMachine/ConsoleApp1/ArrayStyle.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    public delegate void XXX(string tttttt);
+
+    class ATM
+    {
+        public void AddMoney(int money, Action<string> del)
+        {
+            // bal += money;
+            del($"Money added {money}");
+        }
+    }
+
+
+    public class ArrayStyle
+    {
+        public string Decorate(int[] array, Func<int, bool> del)
+        {
+            var sb = new StringBuilder("[ ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (del(array[i]))
+                {
+                    sb.Append($"({array[i]}), ");
+                }
+                else
+                {
+                    sb.Append($"{array[i]}, ");
+                }
+            }
+
+            sb.Append(']');
+            return sb.ToString();
+        }
+    }
+}
---
+++ ATMachine/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new ATM().AddMoney(123, Console.WriteLine);
+
+
+
+        }
+
+        public static void DisplayMessage(string message)
+        {
+            TextBlock
+        }
+    }
+}
